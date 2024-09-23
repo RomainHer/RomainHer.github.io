b780571b28709de396e1e3d82f3bc8e75bb0c008
RomainHer <romain.heriteau1@gmail.com>
2024-09-23 12:26:23 +0200

    add v1 of the changing word

--- src/components/contents/HomeContent.vue
+++ src/components/contents/HomeContent.vue
@@ -1,4 +1,68 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const wordsDescription = ['DEVELOPER', 'ENGINEER', 'HARD WORKER', 'PASSIONATE']
+const colors = ['#40DBEF', '#FD9638', '#4CF75D']
+let writingWord = ref(wordsDescription[0])
+let writingWordIndex = ref(0)
+let writingWordIndexLetter = ref(0)
+let writingWordColorIndex = ref(0)
+let deletingWord = ref(false)
+let interval = ref(100)
+
+onMounted(() => {
+  setInterval(() => {
+    if (deletingWord.value && writingWordIndexLetter.value > 0) {
+      writingWordIndexLetter.value--
+    } else if (deletingWord.value && writingWordIndexLetter.value == 0) {
+      deletingWord.value = false
+      writingWordIndex.value = (writingWordIndex.value + 1) % wordsDescription.length
+      writingWordColorIndex.value = (writingWordColorIndex.value + 1) % colors.length
+    } else if (writingWordIndexLetter.value == wordsDescription[writingWordIndex.value].length) {
+      deletingWord.value = true
+    } else {
+      writingWordIndexLetter.value++
+    }
+    changeWritingWord(writingWordIndex.value, writingWordIndexLetter.value)
+  }, interval.value)
+})
+
+function changeWritingWord(i: number, j: number) {
+  writingWord.value = wordsDescription[i].slice(0, j)
+}
+/*var showText = function (target, word, color, index, interval) {
+    message = wordsDescription[word];
+    if (index < message.length) {
+        document.getElementById(target).append(message[index++]);
+        setTimeout(function () { showText(target, word, color, index, interval); }, interval);
+    } else {
+        setTimeout(function () {
+            deleteText(target, word, color, 50);
+        }, 3000); // Appelle deleteText après la fin de showText
+    }
+}
+
+var deleteText = function (target, word, color, interval) {
+    var text = document.getElementById(target).innerHTML;
+    if (text.length > 0) {
+        document.getElementById(target).innerHTML = text.substring(0, text.length - 1);
+        setTimeout(function () { deleteText(target, word, color, interval); }, interval);
+    } else {
+        word++;
+        color++;
+        if(word == wordsDescription.length) word = 0;
+        if(color == colors.length) color = 0;
+        document.getElementById(target).style.color = colors[color];
+        setTimeout(function () {
+            showText(target, word, color, 0, 100);
+        }, 0);
+    }
+}*/
+
+function changeMessage(i: any, j: any) {
+  throw new Error('Function not implemented.')
+}
+</script>
 
 <template>
   <div id="home-content">
@@ -11,7 +75,11 @@
         <div class="en-des description">
           <div>
             As an engineering student in my final year at Enssat in Brittany, France, and I'm a
-            <span id="word-description-show-hide"></span>.
+            <span
+              id="word-description-show-hide"
+              :style="{ color: colors[writingWordColorIndex] }"
+              >{{ writingWord }}</span
+            >.
           </div>
           <div>
             I enjoy working on personal projects related to web and mobile applications. Recently,
