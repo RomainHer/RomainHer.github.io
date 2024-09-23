5fbd55a518090c7e55d74ec6123b58fce804bb7e
RomainHer <romain.heriteau1@gmail.com>
2024-09-23 18:22:21 +0200

    work on contact form

--- src/components/contents/ContactContent.vue
+++ src/components/contents/ContactContent.vue
@@ -7,9 +7,14 @@
         <div class="contact-title-en">Contact me</div>
         <div class="contact-title-fr">Me contacter</div>
       </div>
-      <div class="contact-form">
-        <form action=""></form>
-      </div>
+      <form action="" class="contact-form">
+        <div>
+          <input type="text" name="name" id="name" placeholder="Name">
+          <input type="text" name="email" id="email" placeholder="Email">
+        </div>
+        <input type="text" name="title" id="title" placeholder="Title">
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="message"></textarea>
+      </form>
     </div>
   </div>
 </template>
@@ -24,13 +29,18 @@
 
 .contact-box {
   margin: 30px;
-  padding: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-radius: 15px;
   background-color: white;
   width: 100%;
   color: black;
   display: flex;
   flex-direction: column;
   align-items: center;
+  -webkit-box-shadow: 0px 0px 25px rgb(0 0 0 / 20%);
+  -moz-box-shadow: 0px 0px 25px rgb(0 0 0 / 20%);
+  box-shadow: 0px 0px 25px rgb(0 0 0 / 20%);
 }
 
 .contact-title {
@@ -58,5 +68,19 @@
   width: fit-content;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  -webkit-box-shadow: 0px -10px 5px rgb(0 0 0 / 10%);
+  -moz-box-shadow: 0px -10px 5px rgb(0 0 0 / 10%);
+  box-shadow: 0px -10px 5px rgb(0 0 0 / 10%);
+}
+
+.contact-form * {
+  width: 80%;
 }
 </style>
