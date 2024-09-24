7239bf61037eb411299de454a5ceb723f00dc257
RomainHer <romain.heriteau1@gmail.com>
2024-09-24 22:50:12 +0200

    install carousel

--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -2,20 +2,20 @@
 import ContactContent from './contents/ContactContent.vue'
 import HomeContent from './contents/HomeContent.vue'
 import MeContent from './contents/MeContent.vue'
-import ProjectContent from './contents/ProjectContent.vue'
+import ProjectsContent from './contents/ProjectsContent.vue'
 </script>
 
 <template>
   <HomeContent />
   <div class="separator"></div>
   <MeContent />
-  <ProjectContent />
+  <ProjectsContent />
   <ContactContent />
 </template>
 
 <style scoped>
 .separator {
   height: 10px;
-  background: linear-gradient(to right, #40DBEF, #30B2EC 42%, #3A5BDC 80%);
+  background: linear-gradient(to right, #40dbef, #30b2ec 42%, #3a5bdc 80%);
 }
 </style>
--- src/components/contents/ProjectContent.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div id="project-page" class="content"></div>
-</template>
-
-<style scoped></style>
